afe7c175bd33600ad70d68e7f3bbe2d92e5a0d09
Ben Whittle <benwhittle31@gmail.com>
2022-07-24 23:06:14 -0700

    tweak SpecEventOrigin type

--- spec/src/lib/types.ts
+++ spec/src/lib/types.ts
@@ -1,11 +1,9 @@
 export interface SpecEventOrigin {
     chainId: number
     blockNumber: number
-    blockHash: string
-    blockTimestamp: number
     transactionHash?: string
     contractAddress: string
-    broadcastTimestamp: number
+    eventTimestamp: number
 }
 
 export type SpecEvent<T> = {
