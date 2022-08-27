d54927d3ff4ad5a014adb8ff97837eea16a574e9
Ben Whittle <benwhittle31@gmail.com>
2022-08-27 17:28:53 -0500

    v0.0.11 -- switch eventTimestamp to ISO string

--- spec/src/lib/events/types.ts
+++ spec/src/lib/events/types.ts
@@ -2,8 +2,8 @@ export interface SpecEventOrigin {
     chainId: number
     blockNumber: number
     transactionHash?: string
-    contractAddress: string
-    eventTimestamp: number
+    contractAddress?: string
+    eventTimestamp: string // ISO string (e.g. "2022-08-28T02:31:37.000Z")
 }
 
 export type SpecEvent<T> = {
